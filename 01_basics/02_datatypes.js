0ec476cddfd90a5f33baca366de4338e69f33218
Sagar Kumar Jha <55592017+sagarjha392@users.noreply.github.com>
2024-01-14 11:50:17 +0000

    "datatypes"

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3 ) // we are using node.js not browser so, it will produce an error 
+
+let name = "Sagar"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standalone value 
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof(undefined));
