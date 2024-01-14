43d57f2a3b7b63f710ecb0c07566b9072d02e2b0
Sagar Kumar Jha <55592017+sagarjha392@users.noreply.github.com>
2024-01-14 12:08:38 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,22 @@
+let score = "false";
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+// "33"=> 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "false";
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(typeof(booleanIsLoggedIn));
+console.log(booleanIsLoggedIn);
+
+// 0 => false; 
+// 1 to any number => true
+// "" => false; 
+// Any string value => true
