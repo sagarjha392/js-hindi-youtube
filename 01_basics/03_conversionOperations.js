f49c9c66d9cefb8122d394b9af899ed6a46b64b2
Sagar Kumar Jha <55592017+sagarjha392@users.noreply.github.com>
2024-01-15 15:30:56 +0000

    operation

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -2,7 +2,7 @@ let score = "false";
 // console.log(typeof score);
 // console.log(typeof (score));
 
-let valueInNumber = Number(score)
+let valueInNumber = Number(score);
 // console.log(typeof valueInNumber);
 // console.log(valueInNumber)
 
@@ -11,12 +11,39 @@ let valueInNumber = Number(score)
 // true => 1; false => 0
 
 let isLoggedIn = "false";
-let booleanIsLoggedIn = Boolean(isLoggedIn)
+let booleanIsLoggedIn = Boolean(isLoggedIn);
 
-console.log(typeof(booleanIsLoggedIn));
-console.log(booleanIsLoggedIn);
+// console.log(typeof(booleanIsLoggedIn));
+// console.log(booleanIsLoggedIn);
 
-// 0 => false; 
+// 0 => false;
 // 1 to any number => true
-// "" => false; 
+// "" => false;
 // Any string value => true
+
+/*********************************************** Operations ************************************************ */
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(`2 + 2 = ${2 + 2}`);
+// console.log(`2 - 2 = ${2 - 2}`);
+// console.log(`2 * 2 = ${2 * 2}`);
+// console.log(`2 / 2 = ${2 / 2}`);
+// console.log(`2 ** 2 = ${2 ** 2}`);
+// console.log(`2 % 2 = ${2 % 3}`);
+
+let str1 = "Hello";
+let str2 = " Sagar";
+let str3 = str1 + str2;
+
+// console.log(str3);
+
+// console.log("1" + 2); //output => 12
+// console.log(1 + "2"); //output => 12
+// console.log("1" + 2 + 2); // output => 122
+// console.log(1 + 2 + "2"); //output => 32
+
+let gameCounter = 100
+
+console.log(++gameCounter);
