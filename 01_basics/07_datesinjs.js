146902ca6f7ae923f7fc3ffd21501b0bfc62a6d7
Sagar Kumar Jha <55592017+sagarjha392@users.noreply.github.com>
2024-01-18 07:43:32 +0000

    datesinjs

---
+++ 01_basics/07_datesinjs.js
@@ -0,0 +1,26 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+let myCreatedDate= new Date(2023,0,23)
+// console.log(myCreatedDate.toDateString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth()+1);
+console.log(newDate.getDay());
+
+newDate.toLocaleString('default',{
+    weekday:"long"
+})
+
+ 
