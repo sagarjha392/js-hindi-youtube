fa8573574a4dda0c326875eb6cafd0d6b7df29d8
Sagar Kumar Jha <55592017+sagarjha392@users.noreply.github.com>
2024-01-18 10:28:44 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,16 @@
+function sayMyName(){
+    console.log("S");
+    console.log("A");
+    console.log("G");
+    console.log("A");
+    console.log("R");
+}
+
+// sayMyName // reference
+// sayMyName() //execution
+
+function addTwoNumbers(number1, number2){
+    return number1+number2;
+}
+
+addTwoNumbers(2, "4")
