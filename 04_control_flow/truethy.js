1100681957ae9b3f343e77dc28f01712bdcc176e
Sagar Kumar Jha <55592017+sagarjha392@users.noreply.github.com>
2024-01-19 15:16:26 +0000

    control flow statements

---
+++ 04_control_flow/truethy.js
@@ -0,0 +1,15 @@
+const userEmail = ()=>{}
+
+if(userEmail){
+    console.log("Got user Email");
+}
+else{
+    console.log("Don't have user email");
+}
+
+// falsy values : false, 0, -0, 0n, "", null, undefined, NaN
+
+// truthy values: "0", "false", " ", [], {}, function(){}, ()=>{}
+
+
+// Nullish Coalscing Operator (??) null defined
