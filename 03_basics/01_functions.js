6f9f238df83628f993f574b10ea6018100351468
Sagar Kumar Jha <55592017+sagarjha392@users.noreply.github.com>
2024-01-19 06:19:18 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -14,3 +14,30 @@ function addTwoNumbers(number1, number2){
 }
 
 addTwoNumbers(2, "4")
+
+function calculateCartPrice (...num1){
+    return num1;
+}
+// console.log(calculateCartPrice(200,400, 500, 2000, 3000));
+const user= {
+    username: "Sagar",
+    price:199,
+}
+
+function handleobject (anyObject){
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+}
+
+// handleobject(user)
+// handleobject({
+//     username: "Sagarjha392",
+//     price: 399
+// })
+
+const myNewArray = [200, 400,600, 100]
+
+function returnSecondValue (getArray){
+    return getArray[1];
+}
+
+// console.log(returnSecondValue(myNewArray));
